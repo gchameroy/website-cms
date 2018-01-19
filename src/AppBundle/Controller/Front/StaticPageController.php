c7bcb663e7b92e4acfebea211565ed929b4a48ba
geoffrey-chameroy <geoffrey.chameroy@gmail.com>
2018-01-19 07:35:22 +0100

    [feature] Render static pages in front

---
+++ src/AppBundle/Controller/Front/StaticPageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller\Front;
+
+use AppBundle\Entity\Newsletter;
+use AppBundle\Entity\StaticPage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StaticPageController extends Controller
+{
+    /**
+     * @Route("/page/{slug}", name="front_static_page")
+     * @Method({"GET"})
+     * @param string $slug
+     * @return Response
+     */
+    public function viewAction(string $slug) {
+        $staticPage = $this->getDoctrine()
+            ->getRepository(StaticPage::class)
+            ->findOnePublished($slug);
+        $this->checkStaticPage($staticPage);
+
+        return $this->render('front/static-page/view.html.twig', [
+            'staticPage' => $staticPage
+        ]);
+    }
+
+    /**
+     * @param StaticPage|null $staticPage
+     */
+    private function checkStaticPage(?StaticPage $staticPage) {
+        if (!$staticPage) {
+            throw $this->createNotFoundException('Static Page Not Found.');
+        }
+    }
+}
