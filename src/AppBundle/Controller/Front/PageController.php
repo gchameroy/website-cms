95dbcae816752e5a24f7aa4a3ab6de9580a44a3f
jennulrich <31631277+jennulrich@users.noreply.github.com>
2018-03-11 17:15:10 +0100

    [Feature] Create page shop (#97)

--- src/AppBundle/Controller/Front/PageController.php
+++ src/AppBundle/Controller/Front/PageController.php
@@ -7,6 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Manager\PartnerManager;
+use AppBundle\Entity\Contact;
+use AppBundle\Form\Type\Contact\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Swift_Mailer;
 
 class PageController extends Controller
 {
@@ -18,6 +22,40 @@ class PageController extends Controller
     public function presentationAction()
     {
         return $this->render('front/page/presentation.html.twig');
+    }
+
+    /**
+     * @Route("/magasin", name="front_shop")
+     * @Method({"GET"})
+     * @return Response
+     */
+    public function shopAtion(Request $request, Swift_Mailer $mailer)
+    {
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            $message = (new \Swift_Message('Demande de contact'))
+                ->setFrom('form-contact@coutellerie-legendre.fr')
+                ->setTo('form-contact@coutellerie-legendre.fr')
+                ->setBody(
+                    $this->renderView('front/emails/contact.html.twig', [
+                        'contact' => $contact
+                    ]), 'text/html'
+                );
+            $mailer->send($message);
+
+            return $this->redirectToRoute('front_home');
+        }
+
+        return $this->render('front/page/shop.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
